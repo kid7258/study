5072898cf65b6321c64895ad156573a204fae5f4
kid7258 <kid7258@gmail.com>
2023-09-20 22:50:59 +0900

    Spring batch project

---
+++ SpringBatchTutorial/src/main/java/com/example/SpringBatchTutorial/job/validatedparam/ValidatedParamJobConfig.java
@@ -0,0 +1,61 @@
+package com.example.SpringBatchTutorial.job.validatedparam;
+
+import com.example.SpringBatchTutorial.job.validatedparam.validator.FileParamValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class ValidatedParamJobConfig {
+    @Autowired
+    private JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    private StepBuilderFactory stepBuilderFactory;
+
+    /**
+     * CompositeJobParameterValidator
+     */
+    @Bean
+    public Job validatedParamJob(Step validatedParamStep) {
+        return jobBuilderFactory.get("validatedParamJob")
+                .incrementer(new RunIdIncrementer())
+                .validator(new FileParamValidator())
+                .start(validatedParamStep)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step validatedParamStep(Tasklet validatedParamTasklet) {
+        return stepBuilderFactory.get("validatedParamStep")
+                .tasklet(validatedParamTasklet)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet validatedParamTasklet(@Value("#{jobParameters['fileName']}") String fileName) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Validated Param Tasklet");
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+}
